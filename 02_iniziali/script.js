13a1bae9a2007b0a931a7cb65ead8d4728296e8c
willymariino <wg.mariino@gmail.com>
2025-03-03 23:15:33 +0100

    finished second snack

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -6,10 +6,22 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 // Dichiara la funzione qui.
 
 function getInitials(names) {
-
+    // creiamo un array vuoto per le iniziali
+    let initials = []
+    // inizializzazione, condizione, incremento
+    for (let i = 0; i < names.lenght; i++)
+        //isolo le iniziali, e poi le spingo nella nuova variabile initials
+        initials.push(names[i].charAT(0))
+    //  initials.push: vado a mettere i nuovi elementi nel nuovo array vuoto della variabile initials, spingendoli dentro dal fondo
+    //   names[i]: indico quale elemento dell'array originario names voglio andare a prendere, utilizzando le parentesi quadre, 
+    //      usando [i] prendo un elemento che passa alla posizione successiva ad ogni ciclo, grazie all'incremento i++
+    //  charAT: indico quale lettera della stringa che compone l'elemento dell'array voglio prendere, 
+    //      con un numero positivo parte dall'inizio, con un numero negativo parte dalla fine della stringa.
+    return initials;
 }
 
 // Invoca la funzione qui e stampa il risultato in console
+console.log(getInitials(names));
 
 
 
