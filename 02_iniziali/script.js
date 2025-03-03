1d1997d1ffc61d2fbf1247c5d97873dc11007cc7
willymariino <wg.mariino@gmail.com>
2025-03-03 23:22:12 +0100

    corrected typos on lenght instead of length, and charAT instead of charAt

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -9,13 +9,13 @@ function getInitials(names) {
     // creiamo un array vuoto per le iniziali
     let initials = []
     // inizializzazione, condizione, incremento
-    for (let i = 0; i < names.lenght; i++)
+    for (let i = 0; i < names.length; i++)
         //isolo le iniziali, e poi le spingo nella nuova variabile initials
-        initials.push(names[i].charAT(0))
+        initials.push(names[i].charAt(0))
     //  initials.push: vado a mettere i nuovi elementi nel nuovo array vuoto della variabile initials, spingendoli dentro dal fondo
     //   names[i]: indico quale elemento dell'array originario names voglio andare a prendere, utilizzando le parentesi quadre, 
     //      usando [i] prendo un elemento che passa alla posizione successiva ad ogni ciclo, grazie all'incremento i++
-    //  charAT: indico quale lettera della stringa che compone l'elemento dell'array voglio prendere, 
+    //  charAt: indico quale lettera della stringa che compone l'elemento dell'array voglio prendere, 
     //      con un numero positivo parte dall'inizio, con un numero negativo parte dalla fine della stringa.
     return initials;
 }
