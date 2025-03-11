bb77136eeabb72c37fff1e288fcc9478c2a5ab34
willymariino <wg.mariino@gmail.com>
2025-03-11 22:19:38 +0100

    commit snack 4

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -1,9 +1,28 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
 
-const word = 'javascript';
+
 
 
 // Dichiara la funzione qui.
+
+const word = stringa
+
+function getvowels(vowels) {
+    let conteggio = 0
+    const vocali = [a, e, i, o, u,]
+    for (let i = 0; i < word.length; i++) {
+        if (vowels.includes(word[i])) {
+            vowels++
+        }
+
+
+        return vowels
+    }
+}
+
+const risultato = getvowels(result)
+console.log("le vocali sono", risultato)
+
 
 
 // Invoca la funzione qui e stampa il risultato in console
